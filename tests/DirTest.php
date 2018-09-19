6e6ef7a114e4ac73f7e809231778d63e184767d8
Willy Micieli <will.micieli@gmail.com>
2018-09-19 10:55:55 +0200

    add missing directory test

---
+++ tests/DirTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+use Imperium\Directory\Dir;
+
+class DirTest extends TestCase
+{
+    public function test_dir_create_and_remove()
+    {
+        $dir = "code";
+        $this->assertTrue(Dir::create($dir));
+        $this->assertTrue(Dir::remove($dir));
+    }
+}
