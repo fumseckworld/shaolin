90d7318017fb1b92cd2debb83ee6290e6352f6cb
Willy Micieli <fumseckworld@gmail.com>
2018-10-06 11:31:48 +0200

    start routing

---
+++ imperium/Router/Router.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Imperium\Router;
+
+class Router {
+
+     
+private $url;
+
+private $routes;
+
+private $named;
+
+public function __construct($url)
+{
+    $this->url = $url;
+    $this->routes = collection();
+    $this->named = collection();
+}
+
+/**
+ * get
+ *
+ * @param  mixed $path
+ * @param  mixed $callable
+ * @param  mixed $name
+ *
+ * @return void
+ * 
+ */
+public function get($path, $callable, $name = null)
+{
+    return $this->add($path, $callable, $name, 'GET');
+}
+
+public function post($path, $callable, $name = null){
+    return $this->add($path, $callable, $name, 'POST');
+}
+
+private function add($path, $callable, $name, $method){
+    $route = new Route($path, $callable);
+    $this->routes[$method][] = $route;
+    if(is_string($callable) && $name === null){
+        $name = $callable;
+    }
+    if($name){
+        $this->namedRoutes[$name] = $route;
+    }
+    return $route;
+}
+
+public function run(){
+    if(!isset($this->routes[$_SERVER['REQUEST_METHOD']])){
+        throw new RouterException('REQUEST_METHOD does not exist');
+    }
+    foreach($this->routes[$_SERVER['REQUEST_METHOD']] as $route){
+        if($route->match($this->url)){
+            return $route->call();
+        }
+    }
+    throw new RouterException('No matching routes');
+}
+
+public function url($name, $params = []){
+    if(!isset($this->namedRoutes[$name])){
+        throw new RouterException('No route matches this name');
+    }
+    return $this->namedRoutes[$name]->getUrl($params);
+}
+
+}
