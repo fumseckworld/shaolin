76015227d69815b05f21900cf11473e0219fbe63
Willy Micieli <micieli@laposte.net>
2018-11-18 12:45:51 +0100

    test form

--- public/index.php
+++ public/index.php
@@ -1,9 +1,13 @@
 <?php
 
- header('Content-Type: application/json');
+ 
 require_once '../vendor/autoload.php';
-
+echo bootswatch('lumen');
+echo fontAwesome();
 whoops();
 $app = instance(\Imperium\Connexion\Connect::POSTGRESQL,'postgres','zen','postgres',5,'dump','base');
 
-echo $app->json()->add($app->show_users(),'users')->add($app->show_databases(),'bases')->add($app->show_tables(),'table')->add($app->all(),'records')->encode();
+echo '<div class="container">';
+echo $app->form(true)->start('/','id','was-validated mt-5','are you sure')->row()->input('text','name','username',fa('fas','fa-user'),'name will be used','name required')->input('number','age','age','','age will be used','age required')->end_row_and_new()->submit('submit','btn-danger btn','id')->end_row()->get();
+
+echo '</div>';
