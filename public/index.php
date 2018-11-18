82d53a2e57de866a681ebc32b66af442ebba2535
Willy Micieli <micieli@laposte.net>
2018-11-18 05:11:01 +0100

    test encode function without json name

--- public/index.php
+++ public/index.php
@@ -6,4 +6,4 @@ require_once '../vendor/autoload.php';
 whoops();
 $app = instance(\Imperium\Connexion\Connect::POSTGRESQL,'postgres','zen','postgres',5,'dump','base');
 
-echo $app->json()->set_name('app.json')->add($app->show_users(),'users')->add($app->show_databases(),'bases')->add($app->show_tables(),'table')->add($app->all(),'records')->encode();
+echo $app->json()->add($app->show_users(),'users')->add($app->show_databases(),'bases')->add($app->show_tables(),'table')->add($app->all(),'records')->encode();
