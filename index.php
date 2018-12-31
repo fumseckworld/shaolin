4ed907f4ade5b7f2a1809f4f4f37233e930f0c89
Willy Micieli <micieli@laposte.net>
2018-12-31 04:29:44 +0100

    remove unseless file

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once 'vendor/autoload.php';
-
-
-echo form('/',id())->save()->input('text','name','username')->input('number','age','age')->textarea('bio','bio',10,10)->submit('a','a','a','a')->get();
