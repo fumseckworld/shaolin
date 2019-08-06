12d1e73e8b46918c3da9047867b093fbe82c288f
Willy Micieli <micieli@laposte.net>
2019-08-06 19:41:44 +0200

    clean index file

--- web/index.php
+++ web/index.php
@@ -7,19 +7,4 @@ chdir(dirname(__DIR__));
 
 require_once 'vendor/autoload.php';
 
-foreach (Routes::where('method',EQUAL,GET)->all() as $route)
-{
-
-    d($route->url);
-    foreach ($routes as $route)
-    {
-        d($route);
-        if ($this->match($route->url))
-        {
-            $this->route = $route;
-            return $this->result();
-        }
-    }
-
-
-}
+app()->run();
