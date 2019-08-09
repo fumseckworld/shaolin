8eecf4bb879d872b610a73b9c6181b8abc4f5931
Willy Micieli <micieli@laposte.net>
2019-08-09 17:04:53 +0200

    add missing tests

--- tests/Model/ModelTest.php
+++ tests/Model/ModelTest.php
@@ -14,6 +14,10 @@ class ModelTest extends Unit
     {
         $this->assertNotEmpty(Users::all());
         $this->assertEquals(100,Users::count());
+        $this->assertNotEmpty(Users::only('id'));
+        $this->assertInstanceOf(\stdClass::class,Users::by('id',1));
+        $this->assertNotEmpty(Users::search('a'));
+        
         $this->assertTrue(Users::destroy(1));
         $this->assertEquals(99,Users::count());
         $this->assertEquals(10,Users::different('id',5)->take(10)->offset(2)->sum());
