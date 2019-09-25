fe64c7c2d29f31bae3106be7be3d441e4a423183
Willy Micieli <micieli@laposte.net>
2019-09-25 10:09:31 +0200

    clean crypt class

--- tests/Crypt/CryptTest.php
+++ tests/Crypt/CryptTest.php
@@ -18,7 +18,9 @@
 			public function test_encrypt_and_decryt()
 			{
 				$this->assertNotEmpty($this->crypt()->encrypt('ALEX'));
+				$this->assertNotEmpty($this->crypt()->encryptString('ALEX'));
 				$this->assertEquals('ALEX',$this->crypt()->decrypt($this->crypt()->encrypt('ALEX')));
+				$this->assertEquals('ALEX',$this->crypt()->decryptString($this->crypt()->encryptString('ALEX')));
 			}
 		}
 	}
