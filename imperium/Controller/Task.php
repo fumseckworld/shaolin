165ddcf25bed9ad937efff165059f99e3d3597c7
Willy Micieli <micieli@laposte.net>
2019-10-25 17:03:33 +0200

    reformat classe

--- imperium/Controller/Task.php
+++ imperium/Controller/Task.php
@@ -1,87 +1,88 @@
 <?php
 
 
-namespace Imperium\Controller;
+namespace Imperium\Controller {
 
 
-use DI\DependencyException;
-use DI\NotFoundException;
-use Imperium\Action\Todo;
-use Imperium\Exception\Kedavra;
-use Imperium\Html\Form\Form;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Response;
-use Twig\Error\LoaderError;
-use Twig\Error\RuntimeError;
-use Twig\Error\SyntaxError;
+    use DI\DependencyException;
+    use DI\NotFoundException;
+    use Imperium\Action\Todo;
+    use Imperium\Exception\Kedavra;
+    use Imperium\Html\Form\Form;
+    use Symfony\Component\HttpFoundation\RedirectResponse;
+    use Symfony\Component\HttpFoundation\Response;
+    use Twig\Error\LoaderError;
+    use Twig\Error\RuntimeError;
+    use Twig\Error\SyntaxError;
 
-class Task extends Controller
-{
-    /**
-     *
-     * Close a task
-     *
-     * @param int $id
-     *
-     * @return RedirectResponse
-     *
-     * @throws DependencyException
-     * @throws NotFoundException
-     * @throws Kedavra
-     *
-     */
-    public function finish(int $id): RedirectResponse
+    class Task extends Controller
     {
-        return Todo::destroy($id) ? $this->back('removed') : $this->back('not removed',false);
-    }
+        /**
+         *
+         * Close a task
+         *
+         * @param int $id
+         *
+         * @return RedirectResponse
+         *
+         * @throws DependencyException
+         * @throws NotFoundException
+         * @throws Kedavra
+         *
+         */
+        public function finish(int $id): RedirectResponse
+        {
+            return Todo::destroy($id) ? $this->back('removed') : $this->back('not removed',false);
+        }
 
-    /**
-     *
-     * Create a task
-     *
-     * @return RedirectResponse
-     *
-     * @throws DependencyException
-     * @throws Kedavra
-     * @throws NotFoundException
-     *
-     */
-    public function add(): RedirectResponse
-    {
-        $todo = collect($this->request()->request->all())->del(CSRF_TOKEN,'method')->all();
-        return Todo::create($todo) ? $this->back('created') : $this->back('fail',false);
-    }
+        /**
+         *
+         * Create a task
+         *
+         * @return RedirectResponse
+         *
+         * @throws DependencyException
+         * @throws Kedavra
+         * @throws NotFoundException
+         *
+         */
+        public function add(): RedirectResponse
+        {
+            $todo = collect($this->request()->request->all())->del(CSRF_TOKEN,'method')->all();
+            return Todo::create($todo) ? $this->back('created') : $this->back('fail',false);
+        }
 
-    /**
-     *
-     * Display all tasks
-     *
-     * @return Response
-     *
-     * @throws DependencyException
-     * @throws Kedavra
-     * @throws NotFoundException
-     * @throws LoaderError
-     * @throws RuntimeError
-     * @throws SyntaxError
-     * 
-     */
-    public function home(): Response
-    {
-        $todo = Todo::query()->by('due',self::ASC)->all();
-        $form = $this->form()->start('task','add')
-            ->row()
-            ->input(Form::TEXT,'task','task')
-            ->end_row_and_new()
-            ->textarea('description','description')
-            ->end_row_and_new()
-            ->input(Form::DATE,'due','The due date')
-            ->end_row_and_new()
-            ->select(false,'priority',['none','low','medium','high'])
-            ->end_row_and_new()
-            ->submit('add')
-            ->end_row()
-            ->get();
-        return $this->view('@todo/home',compact('todo','form'));
+        /**
+         *
+         * Display all tasks
+         *
+         * @return Response
+         *
+         * @throws DependencyException
+         * @throws Kedavra
+         * @throws NotFoundException
+         * @throws LoaderError
+         * @throws RuntimeError
+         * @throws SyntaxError
+         *
+         */
+        public function home(): Response
+        {
+            $todo = Todo::query()->by('due',self::ASC)->all();
+            $form = $this->form()->start('task','add')
+                ->row()
+                ->input(Form::TEXT,'task','task')
+                ->end_row_and_new()
+                ->textarea('description','description')
+                ->end_row_and_new()
+                ->input(Form::DATE,'due','The due date')
+                ->end_row_and_new()
+                ->select(false,'priority',['none','low','medium','high'])
+                ->end_row_and_new()
+                ->submit('add')
+                ->end_row()
+                ->get();
+            return $this->view('@todo/home',compact('todo','form'));
+        }
     }
 }
