2285b3050f45a78cf81746166069ec335af0c607
Willy Micieli <micieli@laposte.net>
2020-02-13 17:58:47 +0000

    update refresh catalog output

--- eywa/Console/Lang/UpdateCatalogues.php
+++ eywa/Console/Lang/UpdateCatalogues.php
@@ -4,10 +4,12 @@
 namespace Eywa\Console\Lang {
 
 
+    use Eywa\Console\Shell;
     use Symfony\Component\Console\Command\Command;
     use Symfony\Component\Console\Input\InputArgument;
     use Symfony\Component\Console\Input\InputInterface;
     use Symfony\Component\Console\Output\OutputInterface;
+    use Symfony\Component\Console\Style\SymfonyStyle;
 
     class UpdateCatalogues extends Command
     {
@@ -21,8 +23,31 @@ namespace Eywa\Console\Lang {
         }
         public function execute(InputInterface $input, OutputInterface $output)
         {
-            collect(config('i18n','locales'))->for([$this,'update'])->ok();
-            return 0;
+            $x = collect();
+            $io = new SymfonyStyle($input,$output);
+
+            $io->title('Create localization catalog');
+            foreach (config('i18n','locales') as $catalogue)
+            {
+                if ($this->update($catalogue))
+                {
+                    $io->success("The $catalogue catalog has been updating successfully");
+                    $x->push(true);
+                }else{
+                    $io->error("The update $catalogue catalog has been fail");
+                    $x->push(false);
+                }
+            }
+
+            if ($x->ok())
+            {
+
+                $io->success('All catalogs has been updated successfully');
+                return 0;
+            }
+            $io->error('Errors has been encontred');
+            return 1;
+
 
         }
 
@@ -38,8 +63,10 @@ namespace Eywa\Console\Lang {
                 $app_version = env('APP_VERSION','1.0');
                 $translator_email = env('TRANSLATOR_EMAIL','translator@free.fr');
 
-                system("xgettext --keyword=_ --language=PHP --add-comments  --msgid-bugs-address=$translator_email --package-version=$app_version  --package-name=$app_name  --sort-output -o $pot $files");
-                system("msgmerge --update $po $pot");
+                if((new Shell("xgettext --keyword=_ --language=PHP --add-comments  --msgid-bugs-address=$translator_email --package-version=$app_version  --package-name=$app_name  --sort-output -o $pot $files"))->run() &&    (new Shell("msgmerge --update $po $pot"))->run())
+                    return true;
+
+                return false;
 
 
             }
