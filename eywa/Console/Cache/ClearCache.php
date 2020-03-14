12fa188c2d50b6018e7ad7341fe50a7b0bcff7ae
Willy Micieli <micieli@laposte.net>
2020-03-14 16:15:42 +0100

    fix clear chache bug

--- eywa/Console/Cache/ClearCache.php
+++ eywa/Console/Cache/ClearCache.php
@@ -8,6 +8,7 @@ namespace Eywa\Console\Cache {
     use Eywa\Cache\FileCache;
     use Eywa\Cache\MemcacheCache;
     use Eywa\Cache\RedisCache;
+    use Eywa\Exception\Kedavra;
     use Symfony\Component\Console\Command\Command;
     use Symfony\Component\Console\Input\InputArgument;
     use Symfony\Component\Console\Input\InputInterface;
@@ -21,13 +22,14 @@ namespace Eywa\Console\Cache {
 
         protected function configure():void
         {
-            $this->setDescription("Clean all cache systems")->addArgument('system', InputArgument::OPTIONAL, 'The cahe system name to clear')->addArgument('mode',InputArgument::OPTIONAL,'The clear mode')->addArgument('interval',InputArgument::OPTIONAL,'The clear interval for watch mode');;
+            $this->setDescription("Clean all cache systems")->addArgument('system', InputArgument::OPTIONAL, 'The cahe system name to clear');
         }
 
         /**
          * @param InputInterface $input
          * @param OutputInterface $output
          * @return int
+         * @throws Kedavra
          */
         public function execute(InputInterface $input, OutputInterface $output)
         {
@@ -36,147 +38,87 @@ namespace Eywa\Console\Cache {
             $x =  strval($input->getArgument('system'));
 
 
-            $mode =  strval($input->getArgument('mode'));
-
-            $time = def($input->getArgument('interval')) ? intval($input->getArgument('interval')) : 20;
-
            if (def($x))
            {
+                if(not_in(['file','redis','apcu','memcache'],$x))
+                {
+                    $io->error(sprintf('The %s system is not currently supported',$x));
+                    return 1;
+                }
 
+               if (equal($x,'file'))
+               {
+                   if ((new FileCache())->clear())
+                       $io->success(sprintf('The %s cache has been removed sucessfully',$x));
+                   else
+                   {
+                       $io->error(sprintf('The %s cache has not been removed',$x));
+                       return 1;
+                   }
+                   return 0;
+               }
+
+                if (equal($x,'redis'))
+                {
+                    if ((new RedisCache())->clear())
+                        $io->success(sprintf('The %s cache has been removed sucessfully',$x));
+                    else
+                    {
+                        $io->error(sprintf('The %s cache has not been removed',$x));
+                        return 1;
+                    }
+                    return 0;
+                }
+
+                if (equal($x,'apcu'))
+                {
+                    if ((new ApcuCache())->clear())
+                        $io->success(sprintf('The %s cache has been removed sucessfully',$x));
+                    else
+                    {
+                        $io->error(sprintf('The %s cache has not been removed',$x));
+                        return 1;
+                    }
+                    return 0;
+                }
+
+                if (equal($x,'memcache'))
+                {
+                    if ((new MemcacheCache())->clear())
+                        $io->success(sprintf('The %s cache has been removed sucessfully',$x));
+                    else
+                    {
+                        $io->error(sprintf('The %s cache has not been removed',$x));
+                        return 1;
+                    }
+                    return 0;
+                }
+               return 0;
+           }else
+           {
                if ((new FileCache())->clear())
-                   $io->success('The file cache has been removed sucessfully');
+                   $io->success('The file cache has been reset successfully');
                else
                    $io->error('The file cache has not been removed');
 
                if ((new RedisCache())->clear())
-                   $io->success('The redis cache has been removed sucessfully');
+                   $io->success('The redis cache has been reset successfully');
                else
                    $io->error('The redis cache has not been removed');
 
                if ((new ApcuCache())->clear())
-                   $io->success('The apcu cache has been removed sucessfully');
+                   $io->success('The apcu cache has been reset successfully');
                else
                    $io->error('The apcu cache has not been removed');
-
                if ((new MemcacheCache())->clear())
-                   $io->success('The memcache cache has been removed sucessfully');
+                   $io->success('The memcache cache has been reset successfully');
                else
                    $io->error('The memcache cache has not been removed');
 
-               $io->success('All cache systems are now empty');
+
                return 0;
            }
-            if (def($x))
-            {
-                if (def($mode) && $mode === 'watch')
-                {
-                    $io->title(sprintf("Clearing the %s cache after every %d seconds of interval",$x,$time));
 
-                    if ($x == 'all')
-                    {
-
-                        while (true)
-                        {
-                            sleep($time);
-
-                            if ((new FileCache())->clear())
-                                $io->success(sprintf('The %s cache has been reset successfully',$x));
-                            else
-                                $io->error('The file cache has not been removed');
-
-                            if ((new RedisCache())->clear())
-                                $io->success(sprintf('The %s cache has been reset successfully',$x));
-                            else
-                                $io->error('The redis cache has not been removed');
-
-                            if ((new ApcuCache())->clear())
-                                $io->success(sprintf('The %s cache has been reset successfully',$x));
-                            else
-                                $io->error('The apcu cache has not been removed');
-                            if ((new MemcacheCache())->clear())
-                                $io->success(sprintf('The %s cache has been reset successfully',$x));
-                            else
-                                $io->error('The memcache cache has not been removed');
-
-                        }
-
-                    }else
-                    {
-                        while (true)
-                        {
-                            sleep($time);
-
-                            switch ($x)
-                            {
-                                case 'redis':
-                                    if((new RedisCache())->clear())
-                                        $io->success(sprintf('The %s cache has been reset successfully',$x));
-                                    else
-                                        $io->error(sprintf('The %s cache has not been reset',$x));
-                                break;
-                                case 'file':
-                                    if((new FileCache())->clear())
-                                        $io->success(sprintf('The %s cache has been reset successfully',$x));
-                                    else
-                                        $io->error(sprintf('The %s cache has not been reset',$x));
-                                break;
-                                case 'apcu':
-                                    if((new ApcuCache())->clear())
-                                        $io->success(sprintf('The %s cache has been reset successfully',$x));
-                                    else
-                                        $io->error(sprintf('The %s cache has not been reset',$x));
-                                break;
-                                case 'memcache':
-                                    if((new MemcacheCache())->clear())
-                                        $io->success(sprintf('The %s cache has been reset successfully',$x));
-                                    else
-                                        $io->error(sprintf('The %s cache has not been reset',$x));
-                                break;
-                                default:
-                                    $io->error('The cache system used is not a valid system');
-                                break;
-                            }
-                        }
-                    }
-                }else{
-
-                    $io->title(sprintf("Clearing the %s cache",$x));
-                    switch ($x)
-                    {
-                        case 'redis':
-                            if((new RedisCache())->clear())
-                                $io->success(sprintf('The %s cache has been reset successfully',$x));
-                            else
-                                $io->error(sprintf('The %s cache has not been reset',$x));
-                        break;
-                        case 'file':
-                            if((new FileCache())->clear())
-                                $io->success(sprintf('The %s cache has been reset successfully',$x));
-                            else
-                                $io->error(sprintf('The %s cache has not been reset',$x));
-                        break;
-                        case 'apcu':
-                            if((new ApcuCache())->clear())
-                                $io->success(sprintf('The %s cache has been reset successfully',$x));
-                            else
-                                $io->error(sprintf('The %s cache has not been reset',$x));
-                        break;
-                        case 'memcache':
-                            if((new MemcacheCache())->clear())
-                                $io->success(sprintf('The %s cache has been reset successfully',$x));
-                            else
-                                $io->error(sprintf('The %s cache has not been reset',$x));
-                        break;
-                        default:
-                            $io->error('The cache system used is not a valid system');
-                        break;
-                    }
-                }
-                $io->success('All cache systems are now empty');
-                return  0;
-            }
-            $io->error('command not work');
-            return 1;
         }
     }
 }
