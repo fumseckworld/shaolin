0f7002da92b5f10b062d37b774227ecd37194aef
Willy Micieli <micieli@laposte.net>
2020-04-03 21:37:32 +0200

    refactor: reformat shell class

--- eywa/Console/Shell.php
+++ eywa/Console/Shell.php
@@ -1,50 +1,46 @@
 <?php
 
-namespace Eywa\Console;
+namespace Eywa\Console {
 
-use Traversable;
-use Symfony\Component\Process\Process;
+    use Symfony\Component\Process\Process;
 
-class Shell
-{
-    /**
-     * @var array<string>
-     */
-    private array $commands;
-    /**
-     * @var Process<mixed>
-     */
-    private Process $process;
-
-    /**
-     * Shell constructor.
-     * @param string $command
-     */
-    public function __construct(string $command)
+    class Shell
     {
-        $this->process = Process::fromShellCommandline($command);
-    }
+        private Process $process;
 
-    /**
-     * @return Process
-     */
-    public function get(): Process
-    {
-        return $this->process;
-    }
-    /**
-     *
-     * Run the thell command
-     *
-     * @param callable|null $callback
-     * @param array<mixed> $env
-     *
-     * @return bool
-     *
-     */
-    public function run(callable $callback = null, array $env = []): bool
-    {
-        $this->process->run($callback, $env);
-        return $this->process->isSuccessful();
+        /**
+         *
+         * Shell constructor.
+         *
+         * @param string $command
+         *
+         */
+        public function __construct(string $command)
+        {
+            $this->process = Process::fromShellCommandline($command);
+        }
+
+        /**
+         * @return Process
+         */
+        public function get(): Process
+        {
+            return $this->process;
+        }
+        /**
+         *
+         * Run the thell command
+         *
+         * @param callable|null $callback
+         * @param array<mixed> $env
+         *
+         * @return bool
+         *
+         */
+        public function run(callable $callback = null, array $env = []): bool
+        {
+            $this->process->run($callback, $env);
+            return $this->process->isSuccessful();
+        }
     }
 }
