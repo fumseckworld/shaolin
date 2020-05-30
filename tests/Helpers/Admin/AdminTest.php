8cb94c841fa7399b8926a208774d63877b03808f
Willy Micieli <micieli@laposte.net>
2020-05-30 13:57:10 +0200

    ci: start travis config

---
+++ tests/Helpers/Admin/AdminTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Testing\Helpers\Admin;
+
+use PHPUnit\Framework\TestCase;
+
+class AdminTest extends TestCase
+{
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
+}
