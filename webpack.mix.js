17676180c262f6fbda4450de91105bdc8dfd2df5
Willy Micieli <micieli@outlook.fr>
2020-07-24 11:25:35 +0200

    feat: add laravel mix to generate css and js

---
+++ webpack.mix.js
@@ -0,0 +1,16 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+
+mix.js('design/js/app.js', 'web/js')
+    .sass('design/sass/app.scss', 'web/css');
